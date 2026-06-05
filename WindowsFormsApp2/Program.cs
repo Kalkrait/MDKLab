16fb0791b5dd23d7ea215cb864a4aa4d0cc302a1
Kalkrait <skaii.kalkrait@gmail.com>
2026-06-05 15:04:07 +0300

    Добавлен класс Employee

--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -3,34 +3,7 @@ using System.Windows.Forms;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-public class Employee
-{
-    public string Name { get; set; }
-    public string Position { get; set; }
-    public DateTime HireDate { get; set; }
-    public DateTime? VacationStart { get; set; }
-    public DateTime? VacationEnd { get; set; }
-    public Employee(string name, string position, DateTime hireDate)
-    {
-        Name = name;
-        Position = position;
-        HireDate = hireDate;
-        VacationStart = null;
-        VacationEnd = null;
-    }
-    public bool IsOnVacation
-    {
-        get
-        {
-            if (VacationStart == null || VacationEnd == null)
-            {
-                return false;
-            }
-            return DateTime.Now >= VacationStart.Value && DateTime.Now <=
-            VacationEnd.Value;
-        }
-    }
-}
+
 public class EmployeeManager
 {
     public List<Employee> Employees { get; private set; }
